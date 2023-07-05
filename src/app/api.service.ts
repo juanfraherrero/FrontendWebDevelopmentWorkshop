3e0a2a8267c9e83af8e3429934d833bdd90c5c61
DavidBurckhardt <dburckhardt@alumnos.exa.unicen.edu.ar>
2023-07-04 21:17:49 -0300

    Agregado de funcion que obtiene datos de la API
    Se muestran los datos de la API en el HTML del componente por defecto

---
+++ src/app/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:3000'; 
+
+  constructor(private http: HttpClient) { }
+
+  getDatos(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/recipes`);
+  }
+
+  // postDatos(datos: any): Observable<any> {
+  //   return this.http.post<any>(`${this.apiUrl}/datos`, datos);
+  // }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recetas-paises';
+  datos: any;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getDatos().subscribe(
+      (response) => {
+        this.datos = response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
