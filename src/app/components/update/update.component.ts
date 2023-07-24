cefb91401258f2f639fe9ab37ac175fb48143584
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2023-07-24 14:36:19 -0300

    Casi Finalizado funcionalidad de update
    Solo Falta la funcionalidad de la imagen, luego esta todo terminado. Hay que continuar con la vista

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -25,6 +25,14 @@ export class updateComponent implements OnInit{
       preparacion: new FormArray([]),
       consejos: new FormArray([])
     })
+    cambios = {
+      nombre: false,
+      descripcion: false,
+      imagen: false,
+      ingredientes: false,
+      preparacion: false,
+      consejos: false
+    }
 
     constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) {}
     
@@ -37,18 +45,14 @@ export class updateComponent implements OnInit{
         this.formUpdate.patchValue({
           nombre: this.receta.nombre
         });
-        this.formUpdate.setControl('descripcion', new FormArray(this.receta.descripcion.map(parrafo => new FormControl(parrafo))))
-        this.formUpdate.setControl('ingredientes', new FormArray(this.receta.ingredientes.map(ingrediente => new FormGroup({
-                                                                                                              nombre: new FormControl(ingrediente.nombre),
-                                                                                                              cantidad: new FormControl(ingrediente.cantidad),
-                                                                                                              opcional: new FormControl(ingrediente.opcional)}))))
-        this.formUpdate.setControl('preparacion', new FormArray(this.receta.preparacion.map(parrafo => new FormControl(parrafo))))
-        this.formUpdate.setControl('consejos', new FormArray(this.receta.consejos.map(parrafo => new FormControl(parrafo))))
+        this.receta.descripcion.forEach(parrafo => this.descripcion.push(new FormControl(parrafo)))
+        this.receta.ingredientes.forEach(ingrediente => this.ingredientes.push(new FormGroup({nombre: new FormControl(ingrediente.nombre), cantidad: new FormControl(ingrediente.cantidad), opcional: new FormControl(ingrediente.opcional)})))
+        this.receta.preparacion.forEach(parrafo => this.preparacion.push(new FormControl(parrafo)))
+        this.receta.consejos.forEach(parrafo => this.consejos.push(new FormControl(parrafo)))
       })
-      console.log(this.formUpdate)
     }
 
-    //Getters
+    // Getters de los array del form
 
     get descripcion(){
       return this.formUpdate.get('descripcion') as FormArray;
@@ -66,52 +70,75 @@ export class updateComponent implements OnInit{
       return this.formUpdate.get('consejos') as FormArray;
     }
 
+    // Creacion y eliminacion de parrafos e Ingredientes
 
     crearParrafo(atributo: string){
-      
+      if (atributo === "descripcion"){
+        this.descripcion.push(new FormControl(''));
+      } else if (atributo === "preparacion"){
+        this.preparacion.push(new FormControl(''));
+      } else {
+        this.consejos.push(new FormControl(''));
+      }
     }
 
-    eliminarParrafo(atributo:string): void{
-      
+    eliminarParrafo(atributo: string): void{
+      if (atributo === "descripcion"){
+        this.descripcion.removeAt(this.descripcion.length-1);
+      } else if (atributo === "preparacion"){
+        this.preparacion.removeAt(this.preparacion.length-1);
+      } else {
+        this.consejos.removeAt(this.consejos.length-1);
+      }
     }
 
     crearIngr(){
-      let ingredientes: Ingredient = {nombre:'', cantidad:'', opcional: true}
-      this.receta.ingredientes.push(ingredientes);
+      const nuevoIngrediente = new FormGroup({nombre: new FormControl(''), cantidad: new FormControl(''), opcional: new FormControl(true)}); 
+      this.ingredientes.push(nuevoIngrediente);
     }
 
     eliminarIngr(){
-      this.receta.ingredientes.pop();
+      this.ingredientes.removeAt(this.ingredientes.length-1);
     }
 
-    verificarParrafos(array: string[], arrayOriginal: string[]): boolean{
-      let tamanio: number = array.length;
+    // Metodos auxiliares
+
+    comparacionDeTextos(arrayOriginal: string[], arrayForm: FormArray): boolean{
+      if (arrayForm.length !== arrayOriginal.length){
+        return true;
+      }
       let i: number = 0;
-      let iguales: boolean = true;
-      while(i < tamanio){
-        if(array[i].length === 0 ){
-          array.splice(i,1);
-          tamanio--;
-        } else {
-          i++;
+      let cambio: boolean = false;
+      while(i < arrayForm.length && ! cambio){
+        if (arrayForm.at(i)['value'] !== arrayOriginal[i]){
+          cambio = true;
         }
+        i++;
       }
-      const tamanioOri = arrayOriginal.length;
-      if (tamanio !== tamanioOri) {iguales = false};
-      i = 0;
-      while(iguales && i < tamanio){
-        console.log(array[i]);
-        if(array[i] === arrayOriginal[i]){
-          i++;
-        } else {
-          iguales = false;
-        }
-      }
-      return iguales;
+      return cambio;
     }
+
+    compararIngredientes(): boolean{
+      if (this.ingredientes.length !== this.receta.ingredientes.length){
+        return true;
+      }
+      let i: number = 0;
+      let cambio: boolean = false;
+      while(i < this.ingredientes.length && ! cambio){
+        if(this.ingredientes.at(i)['value'].nombre !== this.receta.ingredientes[i].nombre ||
+          this.ingredientes.at(i)['value'].cantidad !== this.receta.ingredientes[i].cantidad ||
+          this.ingredientes.at(i)['value'].opcional !== this.receta.ingredientes[i].opcional) {
+            cambio = true;
+        }
+        i++;
+      }
+      return cambio;
+    }
+
+    // Funcion submit del formulario
 
     actualizar(){
-      /*let act : {
+      let act : {
         nombre?: string,
         descripcion? : string[],
         imagen? : string,
@@ -119,14 +146,25 @@ export class updateComponent implements OnInit{
         preparacion? : string[],
         consejos? : string[]
       } = {};
-      if (this.recetaModificada.nombre !== this.receta.nombre){
-        act.nombre = this.recetaModificada.nombre;
+      let redireccion: string = this.nombreReceta
+      if (this.formUpdate.get('nombre')?.value !== this.receta.nombre){
+        act.nombre = this.formUpdate.get('nombre')?.value;
+        redireccion =  this.formUpdate.get('nombre')?.value; 
       }
-      console.log(this.recetaModificada.descripcion);
-      if (! this.verificarParrafos(this.recetaModificada.descripcion, this.receta.descripcion)){
-        act.descripcion = this.recetaModificada.descripcion;
+      if (this.comparacionDeTextos(this.receta.descripcion, this.descripcion)){
+        act.descripcion = this.formUpdate.get('descripcion')?.value;
       }
-      console.log(this.receta.descripcion);
-      this.apiService.updateReceta(this.pais, this.nombreReceta, act).subscribe((): void => {this.router.navigate(['/'+this.pais+'/'+this.nombreReceta]);});*/
+      if (this.compararIngredientes()){
+        act.ingredientes = this.formUpdate.get('ingredientes')?.value;
+      }
+      if (this.comparacionDeTextos(this.receta.preparacion, this.preparacion)){
+        act.preparacion = this.formUpdate.get('preparacion')?.value;
+      }
+      if (this.comparacionDeTextos(this.receta.consejos, this.consejos)){
+        act.consejos = this.formUpdate.get('consejos')?.value;
+      }
+      this.apiService.updateReceta(this.pais, this.nombreReceta, act).subscribe((): void => {
+          this.router.navigate(['/'+this.pais+'/'+redireccion]);
+      });
     }
 }
